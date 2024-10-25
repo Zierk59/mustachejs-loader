9806091d3b0c5da903ad49e9a42ae848852b18f6
Dominik <dominik.kozlik3@interia.pl>
2024-10-25 11:38:32 +0200

    updated dependencies (fix CVE from html-minifer) & added html-minifer-terser & added + updated tests & refactorized loader (migrated to ES from CommonJS, migrated to webpack dynamic import instead of CJS) & migrated to async loader

--- index.js
+++ index.js
@@ -1,103 +1,149 @@
 'use strict';
-var loaderUtils = require('loader-utils');
-var Hogan = require('hogan.js');
-var minifier = require('html-minifier');
 
+import loaderUtils from 'loader-utils';
+import Hogan from 'hogan.js';
+import { minify } from 'html-minifier-terser';
 
-// https://github.com/kangax/html-minifier#options-quick-reference
-var minifierDefaults = {
-    removeComments: true,
-    collapseWhitespace: true,
-    collapseBooleanAttributes: true,
-    removeRedundantAttributes: true,
-    removeEmptyAttributes: true,
-    caseSensitive: true,
-    ignoreCustomFragments: [ /{{.*?}}/ ]
+// TODO: remove loader-utils dependency since this.getOptions and this.stringifyRequest
+//       are standard functions of webpack loaders
+const { getOptions, stringifyRequest } = loaderUtils;
+
+/**
+ * Options available via:
+ * https://github.com/terser/html-minifier-terser?tab=readme-ov-file#options-quick-reference
+ */
+const minifierDefaults = {
+  removeComments: true,
+  collapseWhitespace: true,
+  collapseBooleanAttributes: true,
+  removeRedundantAttributes: true,
+  removeEmptyAttributes: true,
+  caseSensitive: true,
+  ignoreCustomFragments: [/{{.*?}}/]
 };
 
-module.exports = function(source) {
-    var query = loaderUtils.getOptions(this) || {};
-    var hoganOpts = Object.assign({}, query, { asString: true });
-    delete hoganOpts.minify;
-    delete hoganOpts.noShortcut;
-    delete hoganOpts.clientSide;
-    delete hoganOpts.tiny;
-    var render;
-    if (query.render) {
-        if (query.clientSide) {
-            this.callback(new Error('"render" and "clientSide" options are mutually exclusive'));
-        }
-        render = typeof hoganOpts.render === 'function' ? hoganOpts.render() : hoganOpts.render;
-        delete hoganOpts.render;
+export default async function (source) {
+  // const query = this.getOptions() || {};
+  const query = getOptions(this) || {};
+  const hoganOptions = Object.assign(
+    {}, query, { asString: true }
+  );
+
+  delete hoganOptions.minify;
+  delete hoganOptions.noShortcut;
+  delete hoganOptions.clientSide;
+  delete hoganOptions.tiny;
+
+  // render function which will be used in output
+  let render;
+
+  if (query.render) {
+    if (query.clientSide) {
+      this.callback(
+        new Error(
+          '"render" and "clientSide" options are mutually exclusive'
+        )
+      );
     }
+
+    render = typeof hoganOptions.render === 'function'
+      ? hoganOptions.render()
+      : hoganOptions.render;
+
+    delete hoganOptions.render;
+  }
+
+  if (this.cacheable) {
+    this.cacheable();
+  }
+
+  if (query.minify) {
+    let minifierOptions = minifierDefaults;
+
+    if (typeof (query.minify) === 'object') {
+      minifierOptions = Object.assign(
+        {}, minifierOptions, query.minify
+      );
+
+      console.dir(minifierOptions);
+    }
+
+    // minify html with terser minifer
+    source = await minify(source, minifierOptions);
+  }
+
+  let suffix;
+
+  if (query.noShortcut) {
+    suffix = 'return T; }();';
+  }
+  else if (query.render) {
+    suffix = `return T.render(${JSON.stringify(render)});};`;
+  }
+  else {
+    suffix = 'return T.render.apply(T, arguments); };';
+  }
+
+  if (query.clientSide) {
+    return `module.exports = async function() {
+      const H = await import('hogan.js');
+      const Hogan = H.default;
+      const src = ${JSON.stringify(source)};
+
+      const T = Hogan.compile(src, ${JSON.stringify(hoganOptions)});
+      ${suffix}`;
+  }
+
+  if (query.tiny) {
+    return `module.exports = async function() {
+      const H = await import('hogan.js');
+      const Hogan = H.default;
+
+      const T = new Hogan.Template(
+      ${Hogan.compile(source, hoganOptions)});
+      ${suffix}`;
+  }
+
+  return `module.exports = async function() {
+    const H = await import('hogan.js');
+    const Hogan = H.default;
+    const T = new Hogan.Template(
+      ${Hogan.compile(source, hoganOptions)},
+      ${JSON.stringify(source)},
+      Hogan);
+    ${suffix}`;
+};
+
+export function pitch (remainingRequest, precedingRequest, data) {
+  if (remainingRequest.indexOf('!') >= 0) {
+    const query = getOptions(this) || {};
+    const hoganOptions = Object.assign({}, query);
+
+    this.stringifyRequest()
+
+    delete hoganOptions.minify;
+    delete hoganOptions.noShortcut;
+    delete hoganOptions.render;
 
     if (this.cacheable) {
-        this.cacheable();
+      this.cacheable();
     }
 
-    // minify?
-    if (query.minify) {
-        // `?minify`
-        var minifierOptions = minifierDefaults;
-
-        // `?{minify:{...}}`
-        if (Object.prototype.toString.call(query.minify) === '[object Object]') {
-            minifierOptions = Object.assign({}, minifierOptions, query.minify);
-        }
-
-        source = minifier.minify(source, minifierOptions);
-    }
-
-    var suffix;
+    let suffix;
     if (query.noShortcut) {
-        suffix = 'return T; }();';
-    } else if (query.render) {
-        suffix = 'return T.render(' + JSON.stringify(render) + ');};';
-    } else {
-        suffix = 'return T.render.apply(T, arguments); };';
+      suffix = 'return T; }();';
     }
-    if (query.clientSide) {
-        return 'var H = require("hogan.js");\n' +
-            'module.exports = function() { ' +
-            'var src = ' + JSON.stringify(source) + ' \n' +
-            'var T = H.compile(src, ' + JSON.stringify(hoganOpts) + ');\n' + suffix;
-    }
-    if (query.tiny) {
-        return 'var H = require("hogan.js");\n' +
-            'module.exports = function() { ' +
-             'var T = new H.Template(' +
-            Hogan.compile(source, hoganOpts) +
-            ');' + suffix;
+    else {
+      suffix = 'return T.render.apply(T, arguments); };';
     }
 
-    return 'var H = require("hogan.js");\n' +
-           'module.exports = function() { ' +
-           'var T = new H.Template(' +
-           Hogan.compile(source, hoganOpts) +
-           ', ' +
-           JSON.stringify(source) +
-           ', H);' + suffix;
-};
-module.exports.pitch = function(remainingRequest, precedingRequest, data) {
-    if (remainingRequest.indexOf('!') >= 0) {
-        var query = loaderUtils.getOptions(this) || {};
-        var hoganOpts = Object.assign({}, query);
-        delete hoganOpts.minify;
-        delete hoganOpts.noShortcut;
-        delete hoganOpts.render;
-        if (this.cacheable) {
-            this.cacheable();
-        }
-        var suffix;
-        if (query.noShortcut) {
-            suffix = 'return T; }();';
-        } else {
-            suffix = 'return T.render.apply(T, arguments); };';
-        }
-        return 'var result = require(' + loaderUtils.stringifyRequest(this, '!!' + remainingRequest) + ')\n' +
-            'var H = require("hogan.js");\n' +
-            'module.exports = function() {\n' +
-            'var T = H.compile(result, ' + JSON.stringify(hoganOpts) + ');\n' +
-            suffix;
-    }
-};
+    return `module.exports = async function() {
+      const result = await import(${stringifyRequest(this, `!!${remainingRequest}`)});
+      const H = await import('hogan.js');
+      const Hogan = H.default;
+
+      const T = Hogan.compile(result, ${JSON.stringify(hoganOptions)});
+      ${suffix}
+    }`
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,86 +1,140 @@
-var expect = require("chai").expect;
-var mustacheLoader = require("../index");
-var fs = require("fs");
-describe("MustacheLoader", function () {
-    describe("Basic run", function () {
-        it("runs without errors", function (done) {
-            fs.readFile('test/sample.html', 'utf8', function (err, data) {
-                try {
-                    if (err) {
-                        done(err);
-                        return;
-                    }
-                    var results = mustacheLoader.apply({query : {}}, [data]);
-                    var module = {};
-                    eval(results);
-                    expect(module.exports).to.not.be.undefined;
-                    expect(module.exports).to.be.a("function");
-                    var templateResults = module.exports({ a: 5 });
-                    expect(templateResults).to.be.equal("<html>\n\
-    <head></head>\n\
-    <body>\n\
-        <div>5</div>\n\
-    </body>\n\
-</html>")
-                    done();
-                }
-                catch (e) {
-                    done(e);
-                }
-            })
-        });
+import { expect } from "chai";
+import mustacheLoader from "../index.js";
+import { read, readFile } from "fs";
 
-        it("permits dynamic render functions", function (done) {
-            fs.readFile('test/sample.html', 'utf8', function (err, data) {
-                try {
-                    if (err) {
-                        done(err);
-                        return;
-                    }
-                    var render = function () {
-                        return { a: 5 };
-                    };
-                    var results = mustacheLoader.apply({query : { render: render }}, [data]);
-                    var module = {};
-                    eval(results);
-                    expect(module.exports).to.not.be.undefined;
-                    expect(module.exports).to.be.a("function");
-                    var templateResults = module.exports();
-                    expect(templateResults).to.be.equal("<html>\n\
+describe("MustacheJSLoader", function () {
+  describe("Basic run", function () {
+    it("runs without errors", function (done) {
+      readFile('test/sample.html', 'utf8', async function (err, data) {
+        try {
+          if (err) {
+            done(err);
+            return;
+          }
+
+          const output = await mustacheLoader.apply({ query: {} }, [data]);
+          const module = {};
+          eval(output);
+
+          expect(module.exports).to.not.be.undefined;
+          expect(module.exports).to.be.a("function");
+
+          const result = await module.exports({ a: 5 });
+          const expectation = "<html>\n\
     <head></head>\n\
     <body>\n\
         <div>5</div>\n\
     </body>\n\
-</html>")
-                    done();
-                }
-                catch (e) {
-                    done(e);
-                }
-            })
-        });
+</html>\n";
+          expect(result).to.be.equal(expectation)
+          done();
+        }
+        catch (e) {
+          done(e);
+        }
+      })
     });
-    describe("Minify", function () {
-        it("runs quickly", function (done) {
-           fs.readFile('test/sample.html', 'utf8', function (err, data) {
-                try {
-                    if (err) {
-                        done(err);
-                        return;
-                    }
-                    var results = mustacheLoader.apply({query: "?minify"}, [data]);
-                    var module = {};
-                    eval(results);
-                    expect(module.exports).to.not.be.undefined;
-                    expect(module.exports).to.be.a("function");
-                    var templateResults = module.exports({ a: 5 });
-                    expect(templateResults).to.be.equal("<html><head></head><body><div>5</div></body></html>")
-                    done();
-                }
-                catch (e) {
-                    done(e);
-                }
-            })
-        });
+
+    it("permits dynamic render functions", function (done) {
+      readFile('test/sample.html', 'utf8', async function (err, data) {
+        try {
+          if (err) {
+            done(err);
+            return;
+          }
+
+          const render = function () {
+            return { a: 5 };
+          };
+
+          const module = {};
+          const output = await mustacheLoader.apply({
+            query: {
+              render: render
+            }
+          }, [
+            data
+          ]);
+
+          eval(output);
+
+          expect(module.exports).to.not.be.undefined;
+          expect(module.exports).to.be.a("function");
+
+          const result = await module.exports();
+          const expectation = "<html>\n\
+    <head></head>\n\
+    <body>\n\
+        <div>5</div>\n\
+    </body>\n\
+</html>\n";
+
+          expect(result).to.be.equal(expectation)
+          done();
+        }
+        catch (e) {
+          done(e);
+        }
+      })
     });
+  });
+  describe("Minify", function () {
+    it("runs quickly", function (done) {
+      readFile('test/sample.html', 'utf8', async function (err, data) {
+        try {
+          if (err) {
+            done(err);
+            return;
+          }
+
+          const module = {};
+          const output = await mustacheLoader.apply({
+            query: "?minify" }, [data]);
+
+          eval(output);
+
+          expect(module.exports).to.not.be.undefined;
+          expect(module.exports).to.be.a("function");
+
+          const result = await module.exports({ a: 5 });
+          const expected = "<html><head></head><body><div>5</div></body></html>";
+
+          expect(result).to.be.equal(expected)
+          done();
+        }
+        catch (e) {
+          done(e);
+        }
+      })
+    });
+
+    it("do not delete comments", function (done) {
+      readFile('test/sample-comments.html', 'utf8', async function (err, data) {
+        try {
+          if (err) {
+            done(err);
+            return;
+          }
+
+          const output = await mustacheLoader.apply(
+            { query: { minify: { removeComments: false } } }, [data]);
+
+          const module = {};
+          eval(output);
+
+          expect(module.exports).to.not.be.undefined;
+          expect(module.exports).to.be.a("function");
+
+          const result = await module.exports({ a: 5 });
+          const expectation = "<html><head></head><body><!-- This is example comment --><div>5</div></body></html>";
+
+          expect(result).to.be.equal(expectation);
+          done();
+        }
+        catch (e) {
+          done(e);
+        }
+      });
+    })
+  });
 });
